033195c667671ab88f8944e7a51cbfa6f034a7f7
SamOGama <samaragama72@gmail.com>
2024-12-05 09:50:05 -0300

    Adicionei a pasta da Aula 02

---
+++ Aula 02/Aula02.py
@@ -0,0 +1,14 @@
+print ('Buenos Días')
+
+nome_usuario = 'Samara Gama' #tipo > str = string
+idade_usuario = 19 #tipo > int = integer
+altura_usuario = 1.62 #tipo > float = ponto flutuante
+maior_idade = True #ou False - tipo > bool = boolean
+
+print ("Seja bem-vindo", nome_usuario)
+print ("Sua idade é", idade_usuario, "anos")
+print ("Você tem", altura_usuario, "metros")
+print ("Você é maior de idade?", maior_idade)
+
+
+
